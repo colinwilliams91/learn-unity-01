d11eedce27be61876faf08100abac37a424e040c
Colin Williams <colin.williams.dev@gmail.com>
2024-06-23 22:07:56 -0500

    fix: FollowPlayer script gets camera component, sets transform and offset to follow plane correctly

--- challenge_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ challenge_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    public Camera main;
+    public Vector3 offset;
+
+    private void Awake()
+    {
+        if (main == null)
+        {
+            Debug.Log("Camera is null");
+            main = GetComponent<Camera>();
+        }
+
+        if (offset.x != 30 || offset.z != 10)
+        {
+            offset = new Vector3 (30, 0, 10);
+        }
+
+        main.transform.position = plane.transform.position + offset;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = plane.transform.position + offset;
+        main.transform.position = plane.transform.position + offset;
     }
 }
