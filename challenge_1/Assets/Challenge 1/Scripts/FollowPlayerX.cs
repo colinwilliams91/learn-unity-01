272dbfadc359f83a3f119cda39177261dae047aa
Colin Williams <colin.williams.dev@gmail.com>
2024-06-23 22:25:24 -0500

    refactor: check for camera defaults, privatize and set vectors

--- challenge_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ challenge_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -8,7 +8,28 @@ public class FollowPlayerX : MonoBehaviour
     public Camera main;
     public Vector3 offset;
 
+    private Vector3 cameraAngle = new(0, -90, 0);
+    private Vector3 resetAngle = Vector3.zero;
+
     private void Awake()
+    {
+        CheckCameraDefaults();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        main.transform.Rotate(cameraAngle);
+        main.transform.position = plane.transform.position + offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        main.transform.position = plane.transform.position + offset;
+    }
+
+    void CheckCameraDefaults()
     {
         if (main == null)
         {
@@ -18,20 +39,14 @@ public class FollowPlayerX : MonoBehaviour
 
         if (offset.x != 30 || offset.z != 10)
         {
-            offset = new Vector3 (30, 0, 10);
+            Debug.Log("Offset x or z incorrect");
+            offset = new Vector3(30, 0, 10);
         }
 
-        main.transform.position = plane.transform.position + offset;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        main.transform.position = plane.transform.position + offset;
+        if (main.transform.rotation != new Quaternion(0, 0, 0, 0))
+        {
+            Debug.Log("Camera Rotation Incorrect -- Resetting to 0, 0, 0");
+            main.transform.Rotate(resetAngle);
+        }
     }
 }
